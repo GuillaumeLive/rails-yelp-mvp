20f4a286550410a40c1094f834010c4a5ca3fde3
Guillaume Faure <gfaure5@hotmail.com>
2021-02-24 16:51:09 +0100

    exo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "Wanjieming", category: "chinese", address: "taipei", phone_number: "01020304" )
+Restaurant.create(name: "Luigi", category: "italian", address: "roma", phone_number: "01020304" )
+Restaurant.create(name: "Takashimaya", category: "japanese", address: "kyoto", phone_number: "01020304" )
+Restaurant.create(name: "Marcel", category: "french", address: "rodez", phone_number: "01020304" )
+Restaurant.create(name: "La_frite", category: "belgian", address: "bruxelles", phone_number: "01020304" )
